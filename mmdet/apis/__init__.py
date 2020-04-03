2bdcaafd377d93d4c590321a6e5eb91d7d25cfff
Kevin Qi <qiwen@qiwen.name>
2020-04-03 05:19:36 -0700

    add batch infer api

--- mmdet/apis/__init__.py
+++ mmdet/apis/__init__.py
@@ -1,4 +1,4 @@
-from .inference import (async_inference_detector, inference_detector,
+from .inference import (async_inference_detector, inference_detector, inference_batch,
                         init_detector, show_result, show_result_pyplot)
 from .test import multi_gpu_test, single_gpu_test
 from .train import get_root_logger, set_random_seed, train_detector
